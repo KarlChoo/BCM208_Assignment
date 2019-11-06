42c20956206ecb09dd9541c880f44b83db4aa4c6
DomISGay <49142849+DomISGay@users.noreply.github.com>
2019-11-06 12:51:59 +0800

    Initial commit

---
+++ app/src/main/java/com/helplive/bcm208assignment/Util.java
@@ -0,0 +1,10 @@
+package com.helplive.bcm208assignment;
+
+import java.util.ArrayList;
+
+public class Util {
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "MHS_db";
+
+
+}
