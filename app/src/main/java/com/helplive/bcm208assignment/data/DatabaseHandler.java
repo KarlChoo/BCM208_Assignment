f244d9a22af16d8164612777f7a7e3c63188a9df
benjaminchai99 <benjaminchai99@gmail.com>
2019-11-24 20:46:17 +0800

    Merge branch 'master' of https://github.com/KarlChoo/BCM208_Assignment
    # Conflicts:
    #	app/src/main/java/com/helplive/bcm208assignment/MainActivity.java

--- app/src/main/java/com/helplive/bcm208assignment/data/DatabaseHandler.java
+++ app/src/main/java/com/helplive/bcm208assignment/data/DatabaseHandler.java
@@ -218,7 +218,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
             db.insert(Constants.mhstables[0],null,contentValues);
 
         } catch (Exception e) {
-            Log.d("Add Housing Offcier: ", e.getMessage());
+            Log.d("Add Housing Officer: ", e.getMessage());
             e.printStackTrace();
         }
         db.close();
